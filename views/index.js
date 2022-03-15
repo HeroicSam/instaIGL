0bed2cf88191d3c3186b779c009ad3d9dd7c502a
Daniel Yang <Danyang@Daniels-MacBook.fios-router.home>
2022-03-14 22:02:25 -0400

    updates

--- views/index.js
+++ views/index.js
@@ -8,26 +8,6 @@ sendDiv.addEventListener("click", () => {
     let currentMoney = moneyDiv.value
     console.log("Current Money")
     console.log(currentMoney)
-    // make post request to backend with input value
-    // fetch("https://instaigl.herokuapp.com/api/money", {
-    //     method: "POST",
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //         // 'Content-Type': 'application/x-www-form-urlencoded',
-    //       },
-    //     body: JSON.stringify({
-    //         dick: currentMoney
-    //     })
-    // }).then((res) => {
-    //     // log response obj
-    //     let body = res.json()
-
-    //     console.log(body)
-    //     // render to the screen :D
-    //     return body
-    // }).then((data) => {
-    //     messageDiv.append(data)
-    // })
 
     axios.post("/api/money", {
         dick: currentMoney
