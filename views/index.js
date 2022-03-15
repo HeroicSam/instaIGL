9567d76b388c9c75b644bddade888c2d2e5504c3
Daniel Yang <Danyang@Daniels-MacBook.fios-router.home>
2022-03-14 20:53:43 -0400

    Pushin P

---
+++ views/index.js
@@ -0,0 +1,9 @@
+const moneyDiv = document.getElementById("money_value")
+const sendDiv = document.getElementById("send")
+const messageDiv = document.getElementById("message")
+
+
+sendDiv.addEventListener("click", () => {
+    // send whats in the input field to our /api/money
+    console.log("dick")
+})
