f345c0d439fcef262fd6601737ef78a982f8cc3c
HeroicSam <cyuen10@gmail.com>
2022-03-14 21:50:15 -0400

    Adding all the backend stuff

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
+import { t_dust_ecobuy_strat1, t_dust_highbuy_strat1 } from "./data";
+
 const express = require("express")
 const app = express()
 app.use('/', express.static('views'));
 app.use('/images', express.static('images'));
 
 const port = process.env.PORT || 5000
+
+app.post('/api/money', (req, res) =>{
+  console.log(req);
+  const userInput = req.body.dick
+  const fixedUserInput = userInput.lower()
+
+  if (fixedUserInput === 'ecobuy') {
+
+    res.send(t_dust_ecobuy_strat1)
+
+  } else if (fixedUserInput === 'highbuy') {
+
+    res.send(t_dust_highbuy_strat1)
+
+  } else if (fixedUserInput === 'midbuy') {
+
+    res.send('Midbuy strats unavailable.')
+
+  } else if (fixedUserInput === 'forcebuy') {
+
+    res.send('Forcebuy strats unavailable.')
+
+  } else {
+
+    res.send('Please enter ecobuy, forcebuy, midbuy, or highbuy!')
+
+  }
+
+  res.send('Dan is fucking retard!!! 8====D');
+})
 
 app.get('/', (req, res) =>{
     res.sendFile('Hello World!')
